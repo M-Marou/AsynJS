332e9ff843c1ca60843048f3eed198beb899d7ef
Marouane Moumni <MAR@YC.com>
2020-04-13 22:04:26 +0100

    Chall 1 done
    created the loadDate function, and now brings data when ever you press on the button!

---
+++ Défi_1/app.js
@@ -0,0 +1,20 @@
+var button = document.getElementById('button');
+var output = document.getElementById('output');
+
+button.onclick = function loadDate() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (xhttp.readyState == 4) {
+            if(xhttp.status == 200) {
+                document.getElementById('output').textContent = xhttp.responseText;
+                // console.log(xhttp.responseText);
+            }
+            if (xhttp.status == 404){
+                console.log('File not found');
+            }
+        } 
+    };
+    xhttp.open('get', 'data.txt', true);
+    xhttp.send();
+    // console.log(xhttp.readyState)
+};
